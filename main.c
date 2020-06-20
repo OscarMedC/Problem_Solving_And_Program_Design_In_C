8959ecc32e94fe34253990f1f8ff62dc297ecd8e
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-20 20:02:13 +0200

    3

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
 	char character_one, character_two;
 
 	printf("Input two characters> ");
-	scanf_s("%c %c", &character_one, 1, &character_two, 1);
+	scanf_s("%c%c", &character_one, 1, &character_two, 1);
 	printf("\nCharacter one: %c, character two: %c\n", character_one, character_two);
 
 	int n = 15;
@@ -17,6 +17,14 @@ int main() {
 	double area = 25.0;
 
 	printf("The area of a square whose side length is %lfcm is %lf square km.\n", side, area);
+
+	int length = 0;
+	int breadth = 0;
+	int perimeter = 0;
+
+	printf("Enter the length and breadth of the rectangle: ");
+	scanf_s("%d%d", &length, &breadth);
+	printf("The perimeter of the rectangle = %d \n", length * breadth);
 
 	return 0;
 } 
