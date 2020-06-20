ba9d790a773be0b108f4937c5807940a08e2c12a
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-20 19:52:07 +0200

    2.a

--- main.c
+++ main.c
@@ -9,5 +9,9 @@ int main() {
 	scanf_s("%c %c", &character_one, 1, &character_two, 1);
 	printf("\nCharacter one: %c, character two: %c\n", character_one, character_two);
 
+	int n = 15;
+
+	printf("The value of n is %d.", n);
+
 	return 0;
 } 
