128b03e1b7bcc5f8e2609fbd52d6c00452543af1
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-21 14:02:45 +0200

    Uppgift1

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
+/*
+*	Programmer: Oscar Olsson	Date completed: 21/06-20
+*
+*	Stores the values 'x' and 76.1 in seperate memory cells, then displays them.
+*/
 #include <stdio.h>
 
-
 int main() {
+	
+	char c = 'X';
+	double a = 76.1;
 
-	char character_one, character_two;
-
-	printf("Input two characters> ");
-	scanf_s("%c%c", &character_one, 1, &character_two, 1);
-	printf("\nCharacter one: %c, character two: %c\n", character_one, character_two);
-
-	int n = 15;
-
-	printf("The value of n is %d.\n", n);
-
-	double side = 5.0;
-	double area = 25.0;
-
-	printf("The area of a square whose side length is %lfcm is %lf square km.\n", side, area);
-
-	int length = 0;
-	int breadth = 0;
-	int perimeter = 0;
-
-	printf("Enter the length and breadth of the rectangle: ");
-	scanf_s("%d%d", &length, &breadth);
-	printf("The perimeter of the rectangle = %d \n", length * breadth);
+	printf("%c %0.1lf", c, a);
 
 	return 0;
 } 
