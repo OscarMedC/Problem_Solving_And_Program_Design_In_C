9f5fc3e206df4237c069b3e40a5aad25ced02d3e
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-21 15:03:45 +0200

    Formatting numbers

--- main.c
+++ main.c
@@ -1,24 +1,16 @@
 /*
 *	Programmer: Oscar Olsson	Date completed: 21/06-20
-*	Implements the following equation avg = (1 - a)^2 + (oldavg - min) / (max - min) in C
+*	Formatting numbers in C
 */
 #include <stdio.h>
 
-#define ALPHA 0.001
-
 int main() {
 
-	double avg = 0.0;
-	double min = 0.0;
-	double oldavg = 0.0;
-	double max = 0.0;
+	int a = 504;
+	double b = 302.558;
+	double c = -12.31;
 
-	printf("Enter the old average, min and max value> ");
-	scanf_s("%lf%lf%lf", &oldavg, &min, &max);
-
-	avg = ((1 - ALPHA) * 2) + ((oldavg - min) / (max - min));
-
-	printf("The average is %lf \n", avg);
+	printf("%5d%11.2lf%9.1lf", a, b, c);
 
 	return 0;
 } 
