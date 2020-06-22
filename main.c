54986e17e7e46868b8998428efa5bfa58bc1e2a6
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-22 19:53:10 +0200

    Dam

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 /*
 *	Programmer: Oscar Olsson	Date completed: 22/06-20
-*	Program that calculates the taxi fare 
+*	Program that calculates the amount of power produced 
 */
 #include <stdio.h>
-#define FARE_RATE 1.5 // Taxi fare rate at in dollars
+#define G_CONST 9.80 // The gravitational constant 9.80m/s^2
+#define ELECTRIC_E 0.90 // The electrical effiency 90%
 
 int main() {
 
-	double miles = 0.0; // User input, distance in miles
-	double fare = 0.0; // Output, totalt cost
+	double damHeight = 0.0; // User input, height of the dam in meters
+	double cubicMeters = 0.0; // User input, cubic meter of water per second
+	double mass = 0.0; // The mass of the water, 1m^3 = 1000kg
+	double work = 0.0; // The amount of energy created 
 
-	printf("Taxi fare calculator\n");
-	printf("Enter the distance travelled in miles: ");
-	scanf_s("%lf", &miles);
+	printf("Power Production Calculator\n");
+	printf("Enter the dams height> ");
+	scanf_s("%lf", &damHeight);
+	printf("Enter the amount of cubic meters of water> ");
+	scanf_s("%lf", &cubicMeters);
 
-	fare = miles * FARE_RATE;
+	mass = cubicMeters * 1000;
+	work = (mass * G_CONST * damHeight) * ELECTRIC_E;
 
-	printf("Your fare is $%.1lf.\n", fare);
+	printf("The amount of power produced is %.2lfMW\n", work / 1000000 );
+
 
 	return 0;
 } 
