bbad82dd834428b87ca10c28bc805cc47ec44191
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-22 19:09:50 +0200

    Programming Projects 1

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 /*
-*	Programmer: Oscar Olsson	Date completed: 21/06-20
-*	Formatting numbers in C
+*	Programmer: Oscar Olsson	Date completed: 22/06-20
+*	Program that calculates the taxi fare 
 */
 #include <stdio.h>
+#define FARE_RATE 1.5 // Taxi fare rate at in dollars
 
 int main() {
 
-	int age = 0;
+	double miles = 0.0; // User input, distance in miles
+	double fare = 0.0; // Output, totalt cost
 
-	scanf_s("%d", &age);
-	printf("%d", age);
+	printf("Taxi fare calculator\n");
+	printf("Enter the distance travelled in miles: ");
+	scanf_s("%lf", &miles);
+
+	fare = miles * FARE_RATE;
+
+	printf("Your fare is $%.1lf.\n", fare);
 
 	return 0;
 } 
