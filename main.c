441fd5445bd304f3131489c62acd0b63fce78c43
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-22 20:20:24 +0200

    Uppgift 3

--- main.c
+++ main.c
@@ -1,28 +1,25 @@
 /*
 *	Programmer: Oscar Olsson	Date completed: 22/06-20
-*	Program that calculates the amount of power produced 
+*	Program that estimates the temperature in a freezer since a power failure
 */
 #include <stdio.h>
-#define G_CONST 9.80 // The gravitational constant 9.80m/s^2
-#define ELECTRIC_E 0.90 // The electrical effiency 90%
+
 
 int main() {
 
-	double damHeight = 0.0; // User input, height of the dam in meters
-	double cubicMeters = 0.0; // User input, cubic meter of water per second
-	double mass = 0.0; // The mass of the water, 1m^3 = 1000kg
-	double work = 0.0; // The amount of energy created 
+	int hours = 0;
+	int minutes = 0;
+	double time = 0.0;
+	double temperature = 0.0;
 
-	printf("Power Production Calculator\n");
-	printf("Enter the dams height> ");
-	scanf_s("%lf", &damHeight);
-	printf("Enter the amount of cubic meters of water> ");
-	scanf_s("%lf", &cubicMeters);
+	printf("\nEnter how long since the start of the power failure in hrs and minutes> ");
+	scanf_s("%d%d", &hours, &minutes);
 
-	mass = cubicMeters * 1000;
-	work = (mass * G_CONST * damHeight) * ELECTRIC_E;
+	time = (double)hours + (double)minutes / 60;
 
-	printf("The amount of power produced is %.2lfMW\n", work / 1000000 );
+	temperature = ((4 * time * time) / (time + 2)) - 20;
+
+	printf("The temperature in the fridge after %.2lf hours is %.2lf.\n", time, temperature);
 
 	return 0;
 } 
