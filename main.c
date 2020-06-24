85be9a2165f069215d9407486e7509fd503f28e4
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-24 19:55:30 +0200

    Uppgift 6, funktionstest OK

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
 /*
-*	Programmer: Oscar Olsson	Date completed: 22/06-20
-*	Calculates the VTBI (volume to be infused)
+*	Programmer: Oscar Olsson	Date completed: 24/06-20
+*	Predicts the score needed on the final exam to achieva a desired grade
 */
 #include <stdio.h>
 
 int main() {
 
-	float volume = 0.0; // Problem input, amount of fluid in bad (ml)
-	int minutes = 0; // Problem input, the number of minutes it should be infused (min)
-	float rate = 0.0; // Problem output, rate in ml/hour which fluid should be infused
+	char desired_grade = ' ';	// Problem input, desired grade
+	double min_avg = 0.0;
+	double curr_avg = 0.0;
+	double final_percent = 0.0;
+	double score_req = 0.0;
 
-	printf("\nVolume to be infused (ml) => ");
-	scanf_s("%f", &volume);
+	printf("\nEnter desired grade> ");
+	scanf_s("%c", &desired_grade, 1);
+	
+	printf("Enter the minium average required> ");
+	scanf_s("%lf", &min_avg);
 
-	printf("Minutes over which to infuse => ");
-	scanf_s("%d", &minutes);
+	printf("Enter the current average in course> ");
+	scanf_s("%lf", &curr_avg);
 
-	rate = volume / ((float)minutes / 60);
+	printf("Enter how much the final counts as a percentage of the course grade> ");
+	scanf_s("%lf", &final_percent);
 
-	printf("\nVTBI: %.0f ml\n", volume);
-	printf("Rate: %.0f ml\n", rate);
+	score_req = ((min_avg - curr_avg) * (100 / (double)final_percent)) + curr_avg;
+
+	printf("\nYou need a score of %.2lf on the final to get a %c.\n", score_req, desired_grade);
 
 	return 0;
 } 
