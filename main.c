d64e26117776e87dabb7744161a537c655bbe55d
Oscar Olsson <pinnenolsson@hotmail.com>
2020-06-26 22:03:20 +0200

    Övning 12, funktionstest OK

--- main.c
+++ main.c
@@ -1,41 +1,28 @@
 /*
 *	Programmer: Oscar Olsson	Date completed: 26/06-20
-*	Perpendicular Bisector Calculator
+*	Calculates the acceleration of a jet fighter
 */
 #include <stdio.h>
 
 
 int main() {
 
-	double x1_cord = 0.0;
-	double y1_cord = 0.0;
-	
-	double x2_cord = 0.0;
-	double y2_cord = 0.0;
+	double take_off_speed = 0.0;
+	double catapult_distance = 0.0;
+	double acceleration = 0.0;
+	double time = 0.0;
 
-	double slope = 0.0;
-	double x_mid = 0.0;
-	double y_mid = 0.0;
-	double bisector_slope = 0.0;
-	double b = 0.0;
+	printf("\nEnter the take off speed (km/hr) > ");
+	scanf_s("%lf", &take_off_speed);
 
-	printf("\nEnter x1 cord > ");
-	scanf_s("%lf", &x1_cord);
-	printf("\nEnter y1 cord > ");
-	scanf_s("%lf", &y1_cord);
+	printf("\nEnter the catapult distance > ");
+	scanf_s("%lf", &catapult_distance);
 
-	printf("\nEnter x2 cord > ");
-	scanf_s("%lf", &x2_cord);
-	printf("\nEnter y2 cord > ");
-	scanf_s("%lf", &y2_cord);
+	time = (2 * catapult_distance) / take_off_speed;
+	acceleration = take_off_speed / time;
 
-	slope = (y2_cord - y1_cord) / (x2_cord - x1_cord);
-	x_mid = (x1_cord + x2_cord) / 2;
-	y_mid = (y1_cord + y2_cord) / 2;
-	bisector_slope = -1 * slope;
-	b = y_mid - bisector_slope * x_mid;
-
-	printf("\n Equation of the perpendicular bisector -\n Y = %.1lfX + %.1lf \n", bisector_slope, b);
+	printf("\nThe time it takes for a jet to accelerate to its take off speed is %2.2lfs", time);
+	printf("\nThe acceleration of the jet is %.2lfm/s^2", acceleration);
 
 	return 0;
 } 
